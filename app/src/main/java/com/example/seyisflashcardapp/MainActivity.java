403410f06b7ae122f4287950e54aa43a03f8dbfc
bseyi <bseyi39@gmail.com>
2021-02-21 17:48:26 -0600

    additional commit

--- app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
@@ -15,10 +15,22 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
 
 
+
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+
+
+
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
             }
         });
     }
