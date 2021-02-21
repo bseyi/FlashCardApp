11c148ffd35d22c0a53f170788c0bd331ff40fdb
bseyi <bseyi39@gmail.com>
2021-02-21 16:41:41 -0600

    first commit

--- app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.seyisflashcardapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
