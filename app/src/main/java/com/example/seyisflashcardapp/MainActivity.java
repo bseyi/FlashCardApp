ace8c145a7b795acf94a23747d49679c3098befe
bseyi <bseyi39@gmail.com>
2021-03-12 21:35:01 -0600

    Update MainActivity.java

--- app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,50 +16,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
 
+        TextView questionTextView = findViewById(R.id.flashcard_question);
+        TextView answerTextView = findViewById(R.id.flashcard_answer);
+        ImageView addCard = findViewById(R.id.myBtn);
 
+        questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                questionTextView.setVisibility(View.INVISIBLE);
+                answerTextView.setVisibility(View.VISIBLE);
             }
         });
-
-        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
-
-
+        answerTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                questionTextView.setVisibility(View.VISIBLE);
+                answerTextView.setVisibility(View.INVISIBLE);
 
-
-
-
-                    }
-                });
-        findViewById(R.id.icon).setOnClickListener(new View.OnClickListener() {
+            }
+        });
+        addCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
-                MainActivity.this.startActivityForResult(intent, 100);
-
+                MainActivity.this.startActivityForResult(intent ,100);
             }
         });
     }
-
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode== 100 && resultCode == RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+        super.onActivityResult(requestCode,resultCode,data);
+        if (requestCode == 100 && resultCode==RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
             String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
             String string2 = data.getExtras().getString("string2");
-            TextView view = (TextView)findViewById(R.id.enterTextField);
+            TextView view = (TextView)findViewById(R.id.flashcard_question);
             view.setText(string1);
-            TextView view2 = (TextView)findViewById(R.id.answerTextField);
+            TextView view2 = (TextView)findViewById(R.id.flashcard_answer);
             view2.setText(string2);
+
         }
     }
 }
