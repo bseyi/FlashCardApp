7c2c7d1a5295d3ab3eecc951c05901316d7ad241
bseyi <bseyi39@gmail.com>
2021-03-12 21:34:54 -0600

    Update AddCardActivity.java

--- app/src/main/java/com/example/seyisflashcardapp/AddCardActivity.java
+++ app/src/main/java/com/example/seyisflashcardapp/AddCardActivity.java
@@ -10,42 +10,41 @@ import android.widget.ImageView;
 
 public class AddCardActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_card);
-
-        ImageView CancelButton = findViewById(R.id.cancel_button);
-        ImageView SaveButton =findViewById(R.id.save_icon);
-        EditText editQuestion = findViewById(R.id.enterTextField);
-        EditText editAnswer = findViewById(R.id.answerTextField);
 
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_add_card);
+
+
+            ImageView CancleButton = findViewById(R.id.my_cancle_Btn);
+            ImageView SaveButton =findViewById(R.id.my_save_icon);
+            EditText editQuestion = findViewById(R.id.editQuestionField);
+            EditText editAnswer = findViewById(R.id.editAnswerField);
 
 
 
-        CancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {  finish();}
+            CancleButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
 
+//on click listener for the save button
+            SaveButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String question = editQuestion.getText().toString();
+                    String answer = editAnswer.getText().toString();
 
-        });
-
-        //ON CLICK LISTENER
-        SaveButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String question = editQuestion.getText().toString();
-                String answer = editAnswer.getText().toString();
-            Intent data =new Intent();
-            data.putExtra("string1",question);
-            data.putExtra("string2",answer);
-            setResult(RESULT_OK,data);
-            finish();
-
-            }
-        });
+                    Intent data =new Intent();     // create a new Intent, this is where we will put our data
+                    data.putExtra("string1", question);  // puts one string into the Intent, with the key as 'string1'
+                    data.putExtra("string2", answer);   // puts another string into the Intent, with the key as 'string2
+                    setResult(RESULT_OK,data);  // set result code and bundle data for response
+                    finish();    // closes this activity and pass data to the original activity that launched this activity
+                }
+            });
+        }
     }
-
-              }
-
