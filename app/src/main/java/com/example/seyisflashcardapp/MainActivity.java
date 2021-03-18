1a7ca3b4e2821e427e19d8537d3750df37f728d5
bseyi <bseyi39@gmail.com>
2021-03-18 10:20:00 -0500

    Latest commit

--- app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
+++ app/src/main/java/com/example/seyisflashcardapp/MainActivity.java
@@ -8,8 +8,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> allFlashcards;
+    int currentCardDisplayedIndex = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         TextView questionTextView = findViewById(R.id.flashcard_question);
         TextView answerTextView = findViewById(R.id.flashcard_answer);
         ImageView addCard = findViewById(R.id.myBtn);
+        ImageView next = findViewById(R.id.next);
 
         questionTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,18 +53,52 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivityForResult(intent ,100);
             }
         });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (allFlashcards.size() == 0)
+                    return;
+                // advance our pointer index so we can show the next card
+                currentCardDisplayedIndex++;
+
+                // make sure we don't get an IndexOutOfBoundsError if we are viewing the last indexed card in our list
+                if(currentCardDisplayedIndex >= allFlashcards.size()) {
+                    Toast.makeText(getApplicationContext(), "You've reached the end of the cards, going back to start.", Toast.LENGTH_SHORT).show();
+                }
+
+                // set the question and answer TextViews with data from the database
+                allFlashcards = flashcardDatabase.getAllCards();
+                Flashcard flashcard = allFlashcards.get(currentCardDisplayedIndex);
+
+                ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getAnswer());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getQuestion());
+            }
+
+        });
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
+        allFlashcards = flashcardDatabase.getAllCards();
+        if (allFlashcards != null && allFlashcards.size() > 0) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(allFlashcards.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(allFlashcards.get(0).getAnswer());
+        }
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode,resultCode,data);
         if (requestCode == 100 && resultCode==RESULT_OK) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            assert data != null;
             String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
             String string2 = data.getExtras().getString("string2");
             TextView view = (TextView)findViewById(R.id.flashcard_question);
             view.setText(string1);
             TextView view2 = (TextView)findViewById(R.id.flashcard_answer);
             view2.setText(string2);
+            flashcardDatabase.insertCard(new Flashcard(string1, string2));
+            allFlashcards = flashcardDatabase.getAllCards();
 
         }
     }
+
+
 }
